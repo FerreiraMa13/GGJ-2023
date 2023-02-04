2b43337be4a4f81c82b93e9a2d40700b7f5e2575
Callum <callumd305@icloud.com>
2023-02-04 22:20:59 +0000

    Healthfix

--- GGJ 2023/Assets/Scripts/Player.cs
+++ GGJ 2023/Assets/Scripts/Player.cs
@@ -22,8 +22,6 @@ public class Player : MonoBehaviour
     public float jumpMultiplier = 1;
     public float BaseJumpForce = 1000f;
     public float maxJumpCounter = 1;
-    public int maxhealth = 100;
-    public int currenthealth;
     public Healthbar healthbar;
     public Pulse pulse;
     public Image deathscreen;
@@ -40,8 +38,6 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         playerController = new PlayerController();
-        currenthealth = maxhealth;
-        healthbar.SetMaxHealth(maxhealth);
         rb = GetComponent<Rigidbody2D>();
         height = GetComponent<BoxCollider2D>().bounds.extents.y;
         animator = GameObject.FindGameObjectWithTag("PlayerAnim").GetComponent<CharacterAnimations>();
@@ -78,13 +74,13 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        if (currenthealth <= 0 && fading == true)
+        if (health.Health <= 0 && fading == true)
         {
             StartCoroutine(LerpFunction(new Color(deathscreen.color.r,deathscreen.color.g,deathscreen.color.b,0.7f),1));
         }
         else
         {
-            if(currenthealth <= 0)
+            if(health.Health <= 0)
             {
                 fading = true;
                 if (Input.GetKeyDown(KeyCode.R))
@@ -189,9 +185,9 @@ public class Player : MonoBehaviour
 
     void TakeDamage(int damage)
     {
-        currenthealth -= damage;
+        health.Health -= damage;
         pulse.pulse();
-        healthbar.SetHealth(currenthealth);
+        healthbar.SetHealth(health.Health);
         
     }
 }
